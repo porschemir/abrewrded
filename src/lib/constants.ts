6af30e116eebc059adfa31a0d38c00eda11b2eb9
porschemir <emirjaqem1@gmail.com>
2026-07-17 12:10:00 -0400

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const CTA_URL = "https://linkthem.net/aff_c?tl_id=37bc56ac&source=nine";
-export const UNDER_21_URL = "https://linkthem.net/aff_c?offer_id=4439";
+export const CTA_URL = "https://trksy.org/aff_c?offer_id=4861&aff_id=186222";
+export const UNDER_21_URL = "https://trksy.org/aff_c?offer_id=4861&aff_id=186222";
